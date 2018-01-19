b0d624f5a945fb3b823593268d186aa2507d1458
Brandon Metcalf <bwmetcalf@gmail.com>
2018-01-19 15:28:18 -0600

    checkpoint for docker image and starting ansible work

--- bwmetcalf/docker/app.py
+++ bwmetcalf/docker/app.py
@@ -1,18 +1,10 @@
 import os
-import optparse
 from flask import Flask
 from redis import Redis
 
-parser = optparse.OptionParser()
-
-parser.add_option('--redis',
-    action="store", dest="redis",
-    help="redis host", default="")
-
-options, args = parser.parse_args()
-
 app = Flask(__name__)
-redishost = options.redis
+
+redishost = os.environ["REDIS_HOST"]
 redis = Redis(host=redishost, port=6379)
 
 @app.route('/')
